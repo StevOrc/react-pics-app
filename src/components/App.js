bf6e1809e3d0a7869baf82425dadaa7c525e38cf
StevOrc <steven.orcun2@gmail.com>
2020-12-07 16:02:16 +0100

    Ok

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,37 @@ import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
 
 class App extends React.Component {
-  state = { images: [] };
+  state = { images: [], error: null };
+
+  componentDidMount(){
+    this.onSearchSubmit("Paris");
+  }
 
   onSearchSubmit = async (term) => {
-    // console.log("App term = ", term);
     try {
       const { data } = await unsplash.get(`/search/photos`, {
         params: {
           query: term,
         },
       });
-      this.setState({ images: data.results });
+      this.setState({ images: data.results, error: null });
       console.log(data);
     } catch (error) {
-      console.log(error);
+      this.setState({ error: "An error occured" });
     }
+  };
+
+  renderImageListOrerror = () => {
+    const { error } = this.state;
+    if (error) return <h3>{error}</h3>;
+    else return <ImageList images={this.state.images} />;
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
+        {this.renderImageListOrerror()}
       </div>
     );
   }
